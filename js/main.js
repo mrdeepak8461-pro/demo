3327741bb5da1c76b66efb45d55b6e999b887be8
mrdeepak8461-pro <mr.deepakgit8461@gmail.com>
2021-03-19 08:35:29 +0530

    chang banner

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    nav:false,
-    autoplay:true,
-    dots:false,
-    animateOut: 'fadeOut',
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
-});
 
 mainImg = document.getElementById('mainImg');
 
@@ -45,4 +27,32 @@ thumb3.addEventListener("click",function(){
 
 thumb4.addEventListener("click",function(){
     mainImg.src=thumb4Src
+});
+
+const nextIcon = '<img src="img/Assets/next.svg" alt="right">';
+const prevIcon = '<img src="img/Assets/previous.svg" alt="left">';
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    autoplay:true,
+    dots:false,
+    navText:[
+        prevIcon,
+        nextIcon
+    ],
+    animateOut: 'fadeOut',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
 });
